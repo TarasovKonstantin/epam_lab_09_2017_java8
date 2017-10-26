44ab1aa4de731b05a51ff41c919e28b07a13254c
Konstantin Tarasov <konstantin_tarasov@epam.com>
2017-10-26 13:08:28 +0300

    Spliterator exercise: part1 (Rectangle spliterator)

--- spliterator/src/main/java/part1/exercise/RectangleSpliterator.java
+++ spliterator/src/main/java/part1/exercise/RectangleSpliterator.java
@@ -1,49 +1,84 @@
 package part1.exercise;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
+import java.util.stream.StreamSupport;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
     private final int[][] array;
+    private int end;
+    private int start;
+
+
+    public RectangleSpliterator(int[][] array, int start, int end) {
+       super(end - start, Spliterator.IMMUTABLE
+                          | Spliterator.ORDERED
+                          | Spliterator.SIZED
+                          | Spliterator.SUBSIZED
+                          | Spliterator.NONNULL);
+        this.array = array;
+        this.start = start;
+        this.end = end;
+
+    }
 
     public RectangleSpliterator(int[][] array) {
-        super(checkArrayAndCalcEstimatedSize(array), 0);       // TODO заменить
-//       super(estimatedSize, Spliterator.IMMUTABLE
-//                          | Spliterator.ORDERED
-//                          | Spliterator.SIZED
-//                          | Spliterator.SUBSIZED
-//                          | Spliterator.NONNULL);
-        this.array = array;
+        this(array,0, (int) checkArrayAndCalcEstimatedSize(array));
     }
 
     private static long checkArrayAndCalcEstimatedSize(int[][] array) {
-        // TODO
-
+        if (Objects.isNull(array))
+            throw new RuntimeException("array equals null");
         return array.length * array[0].length;
     }
 
     @Override
     public OfInt trySplit() {
-        // TODO
-        throw new UnsupportedOperationException();
+
+        int length = end - start;
+        if(length < 2){
+            return null;
+        }
+
+        int middle = start + length/2;
+        RectangleSpliterator res = new RectangleSpliterator(array, start, middle);
+        start = middle;
+        return res;
     }
 
     @Override
     public long estimateSize() {
-        // TODO
-        throw new UnsupportedOperationException();
+        return end - start;
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        // TODO
-        throw new UnsupportedOperationException();
+
+        if(start < end){
+            int i = array[start / array[0].length][start % array[0].length];
+            ++start;
+            action.accept(i);
+            return true;}
+        return false;
+        }
+
+
+    public static void main(String[] args) {
+        int[][]initArr=new int[][]{{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        long res= StreamSupport.intStream(new RectangleSpliterator(initArr), true)
+                .asLongStream()
+                .sum();
+        System.out.println(res);
+
     }
 
-
 }
-
 
 class A {
 
